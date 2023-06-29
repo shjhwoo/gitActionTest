6d075427a30b7c2db5f55fc0d13998b982f3247a
shjhwoo <108392916+shjhwoo@users.noreply.github.com>
2023-06-29 09:53:59 +0900

    한ㅂ번더 0.0.5

--- main.go
+++ main.go
@@ -5,7 +5,4 @@ import "fmt"
 func main() {
 	fmt.Println("hello world!")
 	fmt.Println(fmt.Sprintf("my name is: %s", "깃액션"))
-	fmt.Println(fmt.Sprintf("my name is: %s", "깃액션233423423"))
-	fmt.Println(fmt.Sprintf("my name is: %s", "깃액션233423423"))
-	fmt.Println(fmt.Sprintf("my name is: %s", "깃액션233423423"))
 }
